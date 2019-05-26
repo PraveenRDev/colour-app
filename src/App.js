774a168c0dbc9aa9e7e3acafb8c96167cebda9db
PraveenRDev <praveenramkumar@outlook.com>
2019-05-26 22:16:31 +0530

    colour helper

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Palette from './Palette';
 import seedColours from './seedColours';
+import { generatePalette } from './colourHelpers';
 
 function App() {
+  console.log(generatePalette(seedColours[5]))
   return (
     <div>
       <Palette {...seedColours[5]} />
